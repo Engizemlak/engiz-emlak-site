98b93f2069a0016d1b5afc80e37ab091f3726212
Engizemlak <salihyaman65@gmail.com>
2026-05-10 22:04:17 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{background:"#000",color:"#fff",minHeight:"100vh",padding:"80px"}}>
+      <h1 style={{fontSize:"64px",color:"#d4af37"}}>
+        ENGİZ EMLAK
+      </h1>
+
+      <p style={{fontSize:"28px"}}>
+        19 Mayıs ve Samsun'un Güvenilir Gayrimenkul Danışmanı
+      </p>
+    </main>
+  )
+}
